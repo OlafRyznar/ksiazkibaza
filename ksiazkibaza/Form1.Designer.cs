68e3e90abb706596014be9570f516756293a12ca
OlafRyznar <91100913+OlafRyznar@users.noreply.github.com>
2024-07-31 13:44:54 +0200

    Dodaj pliki projektów.

---
+++ ksiazkibaza/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ksiazkibaza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Create = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Read = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(68, 40);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(162, 33);
+            this.Create.TabIndex = 0;
+            this.Create.Text = "Create table";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(68, 274);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(162, 33);
+            this.Update.TabIndex = 1;
+            this.Update.Text = "Update table";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(68, 108);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(162, 33);
+            this.Insert.TabIndex = 2;
+            this.Insert.Text = "Insert data";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(68, 366);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(162, 33);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Delete record";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Read
+            // 
+            this.Read.Location = new System.Drawing.Point(68, 186);
+            this.Read.Name = "Read";
+            this.Read.Size = new System.Drawing.Size(162, 33);
+            this.Read.TabIndex = 4;
+            this.Read.Text = "Read from table";
+            this.Read.UseVisualStyleBackColor = true;
+            this.Read.Click += new System.EventHandler(this.Read_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(252, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(479, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(719, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(252, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(662, 306);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(719, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 505);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Read);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Create);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Read;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ ksiazkibaza/Form1.cs
@@ -0,0 +1,96 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ksiazkibaza
+{
+    public partial class Form1 : Form
+    {
+
+        string server = "localhost";
+        string uid = "root";
+        string password = "";
+        string database = "ksiazka";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            string conString = "server="+ server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+            string createTable = "create Table ksiazka(id int, name varchar(50),opis varchar(50),cena int)";
+            MySqlCommand cmd = new MySqlCommand(createTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+
+            // Properly constructing the query with correct quotation marks
+            string createTable = "INSERT INTO ksiazka(id, name, opis, cena) VALUES("
+                                 + textBox1.Text + ", '"
+                                 + textBox2.Text + "', '"
+                                 + textBox3.Text + "', "
+                                 + textBox4.Text + ")";
+
+            MySqlCommand cmd = new MySqlCommand(createTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Read_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+            string createTable = "Select * from ksiazka";
+            MySqlCommand cmd = new MySqlCommand(createTable, con);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+            string createTable = "update ksiazka set name='Harry Potter' where id=2";
+            MySqlCommand cmd = new MySqlCommand(createTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            string conString = "server=" + server + ";uid=" + uid + ";pwd=" + password + ";database=" + database;
+            MySqlConnection con = new MySqlConnection(conString);
+            con.Open();
+            string createTable = "Delete from test_table where id = 2";
+            MySqlCommand cmd = new MySqlCommand(createTable, con);
+            int i = cmd.ExecuteNonQuery();
+            MessageBox.Show(i.ToString());
+        }
+    }
+}
